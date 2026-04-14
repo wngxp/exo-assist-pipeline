017b07e09e80c718306ccecc4ff8819d3139cfe1
wngxp <x253wang@uwaterloo.ca>
2026-04-14 11:02:37 +0800

    Update test_myosuite_ppo.py
    basic ppo with 1.5 mil steps

--- rl/test_myosuite_ppo.py
+++ rl/test_myosuite_ppo.py
@@ -1,23 +1,17 @@
 from myosuite.utils import gym as myogym
 import myosuite
 from stable_baselines3 import PPO
+from stable_baselines3.common.monitor import Monitor
 
 env = myogym.make("myoLegWalk-v0")
+env = Monitor(env)
 
-model = PPO(
-    "MlpPolicy",
-    env,
-    verbose=1,
-    n_steps=2048,
-    batch_size=64,
-    learning_rate=3e-4,
-    device="cpu",
-)
+model = PPO.load("rl_output/walker_policy", env=env, device="cpu")
 
-model.learn(total_timesteps=500_000)
+model.learn(total_timesteps=1_000_000, reset_num_timesteps=False)
 
-model.save("rl_output/walker_policy")
-print("Saved walker to rl_output/walker_policy.zip")
+model.save("rl_output/walker_policy_1p5M")
+print("Saved walker to rl_output/walker_policy_1p5M.zip")
 
 # ===== Step 2: Evaluate =====
 reset_out = env.reset()
@@ -40,7 +34,8 @@ for i in range(2000):
         print(f"step={episode_steps}")
 
     if done:
-        print(f"[EVAL END] step={episode_steps}")
+        print(f"[EVAL END] step={episode_steps}, reward={reward}")
+        print("info:", info)
         reset_out = env.reset()
         obs = reset_out[0] if isinstance(reset_out, tuple) else reset_out
         episode_steps = 0
