a980a0f01f7a253d67b52fc6622fb0b20f228473
wngxp <x253wang@uwaterloo.ca>
2026-04-16 16:37:52 +0800

    Update inspect_data.py

--- rl/scripts/inspect_data.py
+++ rl/scripts/inspect_data.py
@@ -8,16 +8,23 @@ print("Num trials:", dataset.getNumTrials())
 trial = 0
 print("Trial length:", dataset.getTrialLength(trial))
 
-# usually final processed pass = last pass
 num_passes = dataset.getNumProcessingPasses()
 print("Num processing passes:", num_passes)
 
-skel = dataset.readSkel(num_passes - 1)
+skel = dataset.readSkel(num_passes - 1, ignoreGeometry=True)
 print("Num DOFs:", skel.getNumDofs())
+print("Num joints:", skel.getNumJoints())
 
-for i in range(skel.getNumDofs()):
-    print(i, skel.getDof(i).getName())
+print("\nJOINTS:")
+for i in range(skel.getNumJoints()):
+    joint = skel.getJoint(i)
+    print(i, joint.getName())
+
+print("\nDOF NAMES:")
+dofs = skel.getDofs()
+for i, dof in enumerate(dofs):
+    print(i, dof.getName())
     
 frame = dataset.readFrames(trial, 0, 1, num_passes - 1)[0]
-print("pos shape:", len(frame.pos))
-print("vel shape:", len(frame.vel))
+print("pos len:", len(frame.pos))
+print("vel len:", len(frame.vel))
